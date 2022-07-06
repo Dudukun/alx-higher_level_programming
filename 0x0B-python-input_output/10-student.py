2836ca9c0bc993807bab28aa0ae4ad3e8a5a1f83
root <elvine@gmail.com  git config --global user.name Dudukun>
2022-07-06 03:15:42 -0700

    my comms

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
-''' class Student that defines a student
-'''
+"""
+Class Module
+"""
 
 
 class Student:
-    '''module class student
-    '''
+    """
+    Student class
+    """
 
     def __init__(self, first_name, last_name, age):
-        '''method __init__
-        '''
+        """initialize method
+        """
         self.first_name = first_name
         self.last_name = last_name
-        self.age = 
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of student instance
+        args:
+            attrs: attributes
+        return:
+            dictionary
+        """
+        if not attrs:
+            return self.__dict__
+
+        return ({key: value for key, value in self.__dict__.items()
+                 if key in attrs})
