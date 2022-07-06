6d24d22b3a31b39e11748801ad9de6353e6375b1
root <elvine@gmail.com  git config --global user.name Dudukun>
2022-07-06 03:17:51 -0700

    my comms

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,15 +1,37 @@
 #!/usr/bin/python3
-''' class Student that defines a student
-'''
+"""
+Class Module
+"""
 
 
 class Student:
-    '''module class student
-    '''
+    """Student class
+    """
 
     def __init__(self, first_name, last_name, age):
-        '''method __init__
-        '''
+        """initialize method"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of student instance
+        args:
+            attrs: attributes
+        return:
+            na
+        """
+        if not attrs:
+            return vars(self)
+
+        return ({key: value for key, value in self.__dict__.items()
+                 if key in attrs})
+
+    def reload_from_json(self, json):
+        """replace attributes method
+        args:
+            json: json object
+        return:
+            na
+        """
+        self.__dict__.update(json)
