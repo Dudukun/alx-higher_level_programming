6dc070d659a2b702158808731859c7e6bd2d801f
root <elvine@gmail.com  git config --global user.name Dudukun>
2022-07-06 03:22:08 -0700

    fixes

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+"""
+Method to check class
+"""
 
 
 def is_same_class(obj, a_class):
-    """'''function: is_same_class
-    obj: an object
-    a_class: a class
-    Returns: Bool
+    """check for class if same type of object
+    args:
+        obj: object import
+        a_class: class type
+    Return:
+        True or False
     """
-    return type(obj) == a_class
+
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
