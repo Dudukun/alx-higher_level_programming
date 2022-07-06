f7ac1ea56acce5fb960250bc7c91eb81ec9ad138
root <elvine@gmail.com  git config --global user.name Dudukun>
2022-07-06 03:28:49 -0700

    fixes

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 """
-more class base
+Square module
 """
-
-
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-"""
-Square class
-"""
-
-
 class Square(Rectangle):
-    """ Square Class """
+    """class inherits from Rectangle"""
+
     def __init__(self, size):
-        """ instantiation with size """
+        """initialization method"""
+        self.integer_validator('size', size)
         self.__size = size
         super().__init__(self.__size, self.__size)
