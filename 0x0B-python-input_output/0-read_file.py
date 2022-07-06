390895362a68d925b3e7ef4015e4d71f04f282f0
root <elvine@gmail.com  git config --global user.name Dudukun>
2022-07-06 03:01:55 -0700

    my commits

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+Read file function
+"""
+
+
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            print(line, end='')
+    """read a file
+    args:
+        filename: file to manipulate
+    return:
+        na
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        for line in f.read():
+            print(line, end="")
