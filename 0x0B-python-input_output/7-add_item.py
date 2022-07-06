deec33946191ac5be8f5939e60b408ad390d481d
root <elvine@gmail.com  git config --global user.name Dudukun>
2022-07-06 03:13:08 -0700

    a script that adds all arguments to a Python list, and then save them to a file:

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-''' script that adds all arguments to a Python list, and then save them to a file
-'''
-import json
+"""
+script to save and load
+"""
 import sys
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_json = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+file = "add_item.json"
 try:
-    py_list = load_from_json_file(filename)
-except FileNotFoundError:
-    py_list = []
-finally:
-    for el in sys.argv[1:]:
-        py_list.append(str(el))
-    save_to_json_file(py_list, filename)
+    new = load_json(file)
+except (ValueError, FileNotFoundError):
+    new = []
+for args in sys.argv[1:]:
+    new.append(args)
+save_json(new, file)
